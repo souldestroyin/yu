b209329a6fb6dea8cc03dca4d4346f8bc37f8ed2
zhengkaixuan <zhengkaixuan@wangdian.cn>
2022-01-18 15:20:43 +0800

    优化

--- yu-match/index.js
+++ yu-match/index.js
@@ -7,7 +7,7 @@ const file = fs.readFileSync("./input1.txt", "utf-8");
 const arr = file
   .trim()
   .split("\n")
-  .map((o) => o.replace(/^\d+\.(\s)*/, ""));
+  .map((o) => o.replace(/^\d+[\.| \、](\s)*/, ""));
 
 const transedFormConfig = require("./trans.js");
 
@@ -19,8 +19,13 @@ arr.forEach((label) => {
     return;
   }
   res.push(transedFormConfig[label]);
+  delete transedFormConfig[label]
 });
 
-const exportText = res.join("\n");
+const exportText = `${res.join("\n")}
+
+// 没有匹配上的
+${Object.keys(transedFormConfig).map(key => key + ' ' + transedFormConfig[key]).join('\n')}
+`;
 
 fs.writeFileSync("output", exportText, "utf-8");
