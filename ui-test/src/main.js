580a7aaad183b067291b157f3763b68e234fd6b7
zhengkaixuan <zhengkaixuan@wangdian.cn>
2022-02-25 17:29:13 +0800

    test pro

---
+++ ui-test/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+
+// eslint-disable-next-line
+/* eslint-disable */
+import YuUi from 'yu-ui'
+
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+Vue.use(YuUi)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
