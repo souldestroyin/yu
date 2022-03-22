db293a7a804c5f0c15c6fe794f8ac5e6d4baef3a
zhengkaixuan <zhengkaixuan@wangdian.cn>
2022-03-22 17:38:01 +0800

    spa

--- s-spa/base/src/base-root-config.js
+++ s-spa/base/src/base-root-config.js
@@ -6,11 +6,19 @@ registerApplication({
     System.import(
       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
     ),
-  activeWhen: ["/"],
+  activeWhen(location) {
+    return location.pathname === '/app/index'
+  },
 });
 
 registerApplication({
   name: "@app/vue3",
+  app: () => System.import("@app/vue3"),
+  activeWhen: ["/app"]
+});
+
+registerApplication({
+  name: "@app/vue1",
   app: () => System.import("@app/vue3"),
   activeWhen: ["/app"]
 });
