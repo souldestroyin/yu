8d82933b2f46ce97f5fd7ef4da6c21f25c5d6873
zhengkaixuan <zhengkaixuan@wangdian.cn>
2022-04-27 13:37:46 +0800

    dialog

--- yu-manager/src/hooks/useDialog.tsx
+++ yu-manager/src/hooks/useDialog.tsx
@@ -6,26 +6,19 @@ export function useDialog(
 ): [open: () => void, close: () => void, loading: Ref<Boolean>] {
   const loading = ref(false);
 
-  const visible = ref(true);
   let instance: App<Element>;
   const open = () => {
-    instance && instance.unmount();
     instance = createApp(YuDialog, {
       ...options,
       loading,
-      visible,
     });
     instance.mount("#dialog");
   };
 
   const close = () => {
     console.log(instance);
-    visible.value = false;
 
-    console.log(visible);
-    setTimeout(() => {
-      instance.unmount();
-    });
+    instance.unmount();
   };
 
   return [open, close, loading];
